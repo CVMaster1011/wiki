6a992b2518d8f9651b2e2770bb4eace6580e6065
1910354889@qq.com <1910354889@qq.com>
2024-05-09 17:44:34 +0800

    4-6 网站首页开发

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <Button type="primary">Primary Button</Button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
