25db8adbb850dc483a7517aba66db970de47361f
1910354889@qq.com <1910354889@qq.com>
2024-05-09 12:14:53 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/longlong/wiki/service/EbookService.java
+++ src/main/java/com/longlong/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.longlong.wiki.domain.EbookExample;
 import com.longlong.wiki.mapper.EbookMapper;
 import com.longlong.wiki.req.EbookReq;
 import com.longlong.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.longlong.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,15 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
